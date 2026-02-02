6b9da8c796405da6bcda73806332265c5c1d27b7
Denis Savchenko <denya.savchenko@gmail.com>
2026-02-03 00:30:09 +0200

    bot added

--- src/Station.ts
+++ src/Station.ts
@@ -1,6 +1,7 @@
 import { TZDate } from "@date-fns/tz";
 import { format } from 'date-fns';
 import { StationStatus } from "./Types/types.js";
+import DeyeCloudApi from "./DeyeCloudApi.js";
 
 interface StationConstructor {
   wirePower?: number;
@@ -28,11 +29,23 @@ class Station {
     return `${this.status === StationStatus.noGrid ? '🔴' : '🟢'} <b>Статус:</b> ${this.status}\n${this.batterySOC > 20 ? `🔋` : `🪫`} <b>Заряд батареї:</b> ${this.batterySOC}%\n⌚ <b>Оновлено:</b> ${format(new TZDate(this.lastUpdateTime * 1000, 'Europe/Kyiv'), 'dd.MM.yyyy HH:mm')}`;
   }
 
-  public updateStation(params: RefreshStationParams): void {
+  private updateStation(params: RefreshStationParams): void {
     this.wirePower = params.wirePower ?? 0;
     this.batterySOC = params.batterySOC ?? 0;
     this.lastUpdateTime = params.lastUpdateTime ?? 0;
     this.refreshStatus();
+    return;
+  }
+
+  public async refreshStation(deyeCloudApi: DeyeCloudApi): Promise<void> {
+    console.log(`Updating station data...`);
+    const stationData = await deyeCloudApi.getStationData();
+    this.updateStation({ batterySOC: stationData.batterySOC, lastUpdateTime: stationData.lastUpdateTime, wirePower: stationData.wirePower });
+    return;
+  }
+
+  public initAutoRefreshing(params: { deyeCloudApi: DeyeCloudApi, ms: number }): Promise<void> {
+    setInterval(async () => this.refreshStation(params.deyeCloudApi), params.ms);
     return;
   }
 
